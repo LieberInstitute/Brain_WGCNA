b7b73e66e3bb3e2e42df42f34dbdb486088e1a7f
Madhur Parihar <mp8@users.noreply.github.com>
2022-10-24 16:21:04 -0400

    Update and rename Hartl networks.R to prepare_Hartl2021networks_script.R
    Hartl2021 preprocessing

--- combine/prepare_Hartl2021networks_script.R
+++ combine/prepare_Hartl2021networks_script.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 library(magrittr)
 
-#rr = read.table("clipboard-128000", sep = "\t", header = T)
 library(readxl)
 
+####################
+##Prepare Hartl2021 network
+##https://doi.org/10.1038/s41593-021-00887-5
+
+#Reading gene-module assignments from Supplementary Table 1
 path = "C:\\Users\\mpariha1\\Desktop\\upload_to_google_drive\\OneDrive - Johns Hopkins\\Paper new analysis\\Hartl2021\\supp1.xlsx"
 
 net_names = excel_sheets(path)
@@ -11,10 +15,6 @@ net_names = excel_sheets(path)
 dat_all = map_dfc(net_names,~ {          # Read all sheets to list
   dd = as.data.frame(read_excel(path, sheet = .x))[,1:2] %>% set_colnames(c("gene",.x)) %>% tibble::column_to_rownames("gene")
   }) %>% tibble::rownames_to_column("gene")
-
-consensus_genes = readRDS("C:/Users/mpariha1/Desktop/upload_to_google_drive/OneDrive - Johns Hopkins/Shared Data/Giulio's ML code/Enrichments/grch38[PGC125new]/SNPextension/consensus_genes/consensus_gene_list.rds")[c("SCZ_prenatal_positive", "SCZ_postnatal_positive")] %>% unlist
-dat_all = dat_all %>% mutate(inSCZConsensus = ifelse(gene %in% consensus_genes, TRUE, FALSE), .before = gene)
-write.table(dat_all,"clipboard-64000")
 
 out = list()
 for (cl in colnames(dat_all)[3:ncol(dat_all)]){
